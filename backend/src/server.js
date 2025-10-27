b951d2f6ad005538b079a7b99a79dc5d6e6d84c9
Alessandro Carnevale <alecarnevale99@outlook.it>
2025-10-27 10:09:31 +0100

    server.js updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,8 +13,6 @@ const app = express()
 const userRoutes = require('./routes/user')
 const authRoutes = require('./routes/auth')
 
-const testRoute = require('./routes/testAuthStuff')
-
 // Database construction
 const db = new Database(process.env.MONGODB_URI)
 
